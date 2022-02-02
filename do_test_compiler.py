6e588950ba806afca941e5d6dff6a82ac910797d
Chris Wailes <chriswailes@google.com>
2022-02-01 16:10:13 -0800

    Add test_compiler.py script
    This CL adds the test_compiler.py script for use in testing toolchain
    updates in the aosp/master-plus-rust repository.  It will:
    
    1. Unpack a provided prebuilt
    2. Update build/soong/rust/config/globals.go
    3. Invoke `m rust && m` for the provided target
    
    Bug: 166837771
    Test: ./toolchain/android_rust/test_compiler.py --prebuilt_path ./dist/ --target aosp_raven-userdebug in aosp/master-plus-rust
    Change-Id: I2d510e7b01c59f9a4ac661dc853b9daff92986cc

---
+++ do_test_compiler.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import argparse
+import inspect
+from pathlib import Path
+import re
+import shutil
+import subprocess
+import sys
+
+import build_platform
+from paths import (RUST_PREBUILT_PATH, SOONG_PATH)
+from utils import (replace_file_contents, run_quiet_and_exit_on_failure, VERSION_PATTERN)
+
+TEST_VERSION_NUMBER: str = "9.99.9"
+
+#
+# Program logic
+#
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=inspect.getdoc(sys.modules[__name__]))
+
+    parser.add_argument(
+        "--prebuilt-path", type=Path, required=True,
+        help="Path to either the build artifact or the directory that contains it")
+    parser.add_argument(
+        "--target", type=str, required=True,
+        help="Device target to build for")
+
+    return parser.parse_args()
+
+
+def main() -> None:
+  args = parse_args()
+
+  # Resolve prebuilt path
+  prebuilt_archive: Path = args.prebuilt_path
+  if prebuilt_archive.exists():
+    if prebuilt_archive.is_dir():
+      files = list(prebuilt_archive.glob("*"))
+      if len(files) != 1:
+        raise RuntimeError(f"Expected 1 file in prebuilt path, instead found {len(files)}")
+
+      prebuilt_archive = files.pop()
+
+    prebuilt_archive = prebuilt_archive.resolve()
+
+  else:
+    raise RuntimeError(f"Path {args.prebuilt_path} does not exist")
+
+  # Prepare host/version path
+  target_and_version_path: Path = RUST_PREBUILT_PATH / build_platform.prebuilt() / TEST_VERSION_NUMBER
+  if target_and_version_path.exists():
+    print("Test prebuilt directory already exists.  Deleting contents.")
+    shutil.rmtree(target_and_version_path, ignore_errors=True)
+
+  target_and_version_path.mkdir()
+
+  # Unpack prebuilt
+  print(f"Extracting archive {prebuilt_archive}")
+  run_quiet_and_exit_on_failure(
+      f"tar -xzf {prebuilt_archive}",
+      f"Failed to extract prebuilt archive",
+      cwd=target_and_version_path)
+
+  # Run 'm rust && m' for build target
+  ENVSETUP_PATH = Path.cwd() / "build" / "envsetup.sh"
+  subprocess.run(
+    f"source {ENVSETUP_PATH} && lunch {args.target} && " +
+      f"RUST_PREBUILTS_VERSION={TEST_VERSION_NUMBER} m rust && m",
+    shell=True, stderr=subprocess.STDOUT)
+
+if __name__ == "__main__":
+    main()
---
+++ test_compiler.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Copyright (C) 2022 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import subprocess
+import sys
+
+SCRIPT_NAME = 'do_test_compiler.py'
+THIS_DIR = os.path.realpath(os.path.dirname(__file__))
+
+def get_host_tag():
+    if sys.platform.startswith('linux'):
+        return "linux-x86"
+    elif sys.platform.startswith('darwin'):
+        return "darwin-x86"
+    else:
+        raise RuntimeError('Unsupported host: {}'.format(sys.platform))
+
+python_bin = os.path.join(THIS_DIR, "..", "..", "prebuilts", "python", get_host_tag(), 'bin', 'python3')
+python_bin = os.path.abspath(python_bin)
+
+subprocess.call(
+        [python_bin, os.path.join(THIS_DIR, SCRIPT_NAME)] + sys.argv[1:])
