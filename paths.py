b44fa310187c58b43ab721397c200d5345fda934
Chris Wailes <chriswailes@google.com>
2022-02-25 00:13:22 +0000

    Revert "Update clang to clang-r445002"
    This reverts commit 69dbc1a09c59f539d84bb6180d2a63de0f3a7c4d.
    
    Reason for revert: The new compiler causes a link error w/ 1.59 source
    
    Bug: 215232614
    Bug: 221314402
    Change-Id: Id346a39b703091560ef54f58ef83d2b6cc236433

--- paths.py
+++ paths.py
@@ -19,7 +19,7 @@ from pathlib import Path
 import build_platform
 
 RUST_VERSION_STAGE0: str = '1.58.1'
-CLANG_REVISION:      str = 'r445002'
+CLANG_REVISION:      str = 'r437112b'
 CLANG_NAME:          str = 'clang-{0}'.format(CLANG_REVISION)
 GLIBC_VERSION:       str = '2.17-4.8'
 GLIBC_SUBVERSION:    str = '4.8.3'
