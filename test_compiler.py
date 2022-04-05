eceb4e73f0cba3891889844caea3e7755da5ce69
Chris Wailes <chriswailes@google.com>
2022-04-05 10:57:15 -0700

    Removed call to `which('bash')`
    The result returned by `which('bash')` on the build bots is unable to
    parse build/envsetup.sh.  This CL switches the `shell` value in a
    `subprocess.run()` call back to `True`.
    
    Test: ./toolchain/android_rust/test_compiler.py --prebuilt-path dist/rust-dist.tar.gz --target aosp_raven-userdebug
    Change-Id: I8f762d16ee64b61ebfd9d2dfc4a4533cf4f77a6b

--- test_compiler.py
+++ test_compiler.py
@@ -116,7 +116,7 @@ def run_build_command(target: str, command: str) -> int:
     return subprocess.run(
         f". {ENVSETUP_PATH} && lunch {target} && " +
         f"RUST_PREBUILTS_VERSION={TEST_VERSION_NUMBER} {command}",
-        shell=shutil.which("bash"), stderr=subprocess.STDOUT).returncode
+        shell=True, stderr=subprocess.STDOUT).returncode
 
 
 def build_rust_artifacts(target: str) -> int:
